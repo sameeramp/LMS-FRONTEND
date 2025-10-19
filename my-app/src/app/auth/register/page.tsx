81cc6ab787b25d5463e3b2116f7f655a7c529015
sameeramp <sameeramp2000@gmail.com>
2025-10-19 17:02:17 +0530

    Refactored

--- my-app/src/app/auth/register/page.tsx
+++ my-app/src/app/auth/register/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
-  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
@@ -30,9 +30,9 @@ export default function RegisterPage() {
       <form onSubmit={handleRegister}>
         <input
           type="text"
-          placeholder="Full Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          placeholder="Username Name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           required
         />
         <input
