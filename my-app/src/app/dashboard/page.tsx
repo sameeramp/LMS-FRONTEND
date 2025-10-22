c1606efc1db9247652b53b22488218c06d1f92ba
sameeramp <sameeramp2000@gmail.com>
2025-10-22 18:10:33 +0530

    Data added in Dashboard

--- my-app/src/app/dashboard/page.tsx
+++ my-app/src/app/dashboard/page.tsx
@@ -5,72 +5,80 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import Navbar from "@/components/Navbar";
 import "../../app/globals.css";
+import { FiArrowUpRight } from "react-icons/fi";
 
 export default function DashboardPage() {
-    const [data, setData] = useState([]);
-    const router = useRouter();
-    useEffect(() => {
-        const user = localStorage.getItem("user");
-        const token = user ? JSON.parse(user).userDetails?.token : null;
-        if (!token) {
-            console.error("No token found, redirecting to login.");
-        }
-        axios
-            .get(`${process.env.NEXT_PUBLIC_API}/course/get-all`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            .then((response) => {
-                setData(response?.data?.course);
-            })
-            .catch((error) => {
-                console.error("Error fetching dashboard stats:", error);
-            });
-    }, []);
-
-    const handleChange = (_id: string) => {
-        router.push(`/dashboard/${_id}`);
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState<any>({});
+  const router = useRouter();
+  useEffect(() => {
+    const username = localStorage.getItem("user") || "";
+    const userDetails = user ? JSON.parse(username).userDetails : ""
+    setUser(userDetails);
+    const token = user ? JSON.parse(username).userDetails?.token : null;
+    if (!token) {
+      console.error("No token found, redirecting to login.");
+      router.push('/auth/login');
     }
+    if (userDetails) {
+      //           axios
+      // .get(`${process.env.NEXT_PUBLIC_API}/user/${userDetails?.id}/stats`, {
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //   },
+      // })
+      // .then((response) => {
+      //   const stats =  response?.data?.stats;        
+      //   if(stats.length>0) setData(stats);
+      // })
+      // .catch((error) => {
+      //   console.error("Error fetching dashboard stats:", error);
+      // });
+      axios
+        .get(`${process.env.NEXT_PUBLIC_API}/course/get-all`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setData(response?.data?.course);
+        })
+        .catch((error) => {
+          console.error("Error fetching dashboard stats:", error);
+        });
+    }
+  }, []);
 
-    return (
-        <>
-            <Navbar />
-            <div
-                style={{
-                    display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "82px",
+  console.log(data, 'data');
 
-                    padding: "20px",
-                    justifyContent: "space-between"
+  const handleChange = (courseId: string) => {
+    router.push(`/dashboard/${courseId}`);
+  };
 
-                }}
-
-                className="dashboard-grid">
-
-                {data ? (
-                    data?.map((course: any) => (
-                        <div key={course._id} className="dashboard-card"
-
-                            onClick={() => handleChange(course?._id)}
-
-
-                            style={{
-                                border: "1px solid #ccc", borderRadius: "8px", padding: "16px",
-                                backgroundColor: "#f9f9f9",
-                                boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"
-                            }}
-
-                        >
-                            <h3>{course?.course}</h3>
-                            <p>{course?.createdAt}</p>
-
-                        </div>
-                    ))
-                ) : (
-                    <p>Loading...</p>
-                )}
+  return (
+    <>
+      <Navbar />
+      <div className="dashboard-grid">
+        <h1>Hi {user?.username}</h1>
+        {data ? (
+          data?.map((course: any) => (
+            <div key={course._id} className="dashboard-card">
+              <button onClick={() => {
+                handleChange(course?._id);
+              }}>
+                Show The Progress
+                <FiArrowUpRight
+                  size={20}
+                />
+              </button>
+              <h3>{course?.course}</h3>
+              <p>{course?.createdAt}</p>
             </div>
-        </>
-    );
-
-};
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
+}
