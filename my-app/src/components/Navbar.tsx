a2a910fce2ba87593a917fc9cd7e92c886301015
sameeramp <sameeramp2000@gmail.com>
2025-10-22 19:37:44 +0530

    Added token logic in navbar

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
 import "../app/globals.css";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+  const [token, setToken] = useState<string | null>(null);
+  useEffect(() => {
+    const user = localStorage.getItem("user") || "";
+    const token = user ? JSON.parse(user).userDetails?.token : null;
+    setToken(token);
+  }, [])
   return (
     <nav className="navbar">
-      <h2>LMS App</h2>
-      <div>
+      {token ? <div>
+        <Link href="/dashboard">  <h2>LMS App</h2></Link>
+        <Link href="/profile">Profile</Link>
+      </div> : <div>
         <Link href="/auth/login">Login</Link>
         <Link href="/auth/register">Register</Link>
-      </div>
+      </div>}
     </nav>
   );
 }
