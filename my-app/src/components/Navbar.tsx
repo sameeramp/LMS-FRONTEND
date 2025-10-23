ec8d1cd0913f750b27f5b78fd78579f79887822f
sameeramp <sameeramp2000@gmail.com>
2025-10-23 08:04:39 +0530

    Added logout functionality

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,19 +1,28 @@
 import Link from "next/link";
 import "../app/globals.css";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const [token, setToken] = useState<string | null>(null);
+  const router = useRouter();
   useEffect(() => {
     const user = localStorage.getItem("user") || "";
     const token = user ? JSON.parse(user).userDetails?.token : null;
     setToken(token);
   }, [])
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setToken(null);
+    router.push("/auth/login");
+  }
   return (
     <nav className="navbar">
       {token ? <div style={{display:"flex", justifyContent:"space-between", width:"100%"}}>
         <Link href="/dashboard">  <h2>LMS App</h2></Link>
         <Link href="/profile">Profile</Link>
+        <a onClick={logout} >Logout</a>
       </div> : <div>
         <Link href="/auth/login">Login</Link>
         <Link href="/auth/register">Register</Link>
